dfccea78191f3361dc01c8a67b5ea3fc6c178225
JamesGames7 <crask26@pickeringcollege.on.ca>
2026-01-28 20:02:22 -0500

    Bank card display

--- modules/js/Game.js
+++ modules/js/Game.js
@@ -5,6 +5,15 @@ class Game {
     setup(gamedatas) {
         this.gamedatas = gamedatas;
         this.setupNotifications();
+        Object.entries(gamedatas.players).forEach(el => {
+            let info = el[1];
+            $('game_play_area').insertAdjacentHTML("beforeend", /*html*/ `
+                <div id="player_area_${info.id}" class="player_area whiteblock">
+                    <div id="player_area_name" class="player_area_name">${this.bga.players.getFormattedPlayerName(parseInt(info.id))}</div>
+                    <div id="bank-${info.id}" class="bank"></div>
+                </div>
+            `);
+        });
     }
     onEnteringState(stateName, args) {
     }
--- src/villagepillage.ts
+++ src/villagepillage.ts
@@ -7,8 +7,19 @@ export class Game {
     }
     
     public setup(gamedatas: VillagePillageGamedatas) {
-        this.gamedatas = gamedatas; 
+        this.gamedatas = gamedatas;
         this.setupNotifications();
+
+        Object.entries(gamedatas.players).forEach(el => {
+            let info: VillagePillagePlayer = el[1];
+
+            $('game_play_area').insertAdjacentHTML("beforeend", /*html*/`
+                <div id="player_area_${info.id}" class="player_area whiteblock">
+                    <div id="player_area_name" class="player_area_name">${this.bga.players.getFormattedPlayerName(parseInt(info.id))}</div>
+                    <div id="bank-${info.id}" class="bank"></div>
+                </div>
+            `)
+        })
     } 
 
     public onEnteringState(stateName: string, args: any) {
